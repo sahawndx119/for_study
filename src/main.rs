edf2ab00f164b0cb6a2ad2908291b5d54fefb5c5
Sahand Salehi <sahawndx@gmail.com>
2025-06-23 12:32:20 +0330

    i added multiple lines to the the main.rs that it get another input and print it as new information from the user !!

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 //in the name of God
 
 fn main () {
-    let mut imput = String::new();
-    std::io::stdin().read_line(&mut imput).unwrap();
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
 
-    println!("hello dear {imput}");
+    println!("hello dear {}" , input.trim());
+
+
+    let mut input2 = String::new();
+    std::io::stdin().read_line(&mut input2).unwrap();
+
+    println!("Got another information from you! it is >> {}" , input2.trim())
 }
