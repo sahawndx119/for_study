d098a10c3ca96f63da61455ab9d459926fb708e4
Sahand Salehi <sahawndx@gmail.com>
2025-06-23 12:57:45 +0330

    i added a fuction to the library that get the age of the user!

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,14 @@
 //in the name of God
 //this library is for traits and functions!
+
+
+
+
+pub fn get_age() {
+    let mut age = String::new();
+    std::io::stdin().read_line(&mut age).unwrap();
+
+    let age = age.trim().parse::<u32>().unwrap();
+
+    println!("got your age ! age >> {age}");
+}
